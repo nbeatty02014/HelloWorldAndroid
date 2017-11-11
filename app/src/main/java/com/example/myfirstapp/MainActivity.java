91744f0ad9e5ecd6fa2308ba39d761e85b6e8f4f
Nick <ncb02014@mymail.pomona.edu>
2017-11-10 22:09:31 -0800

    toggle status

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ToggleButton;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean status = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        final ToggleButton button = findViewById(R.id.toggleButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                if(status){
+                    status = false;
+                }
+                else{
+                    status = true;
+                }
+            }
+        });
+
+
+
     }
 
     public boolean doNothing(){
